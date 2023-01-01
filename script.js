40438e1d48ccddda56119618398024cd22c15c92
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-01 18:48:26 +0530

    files changed

--- script.js
+++ script.js
@@ -3,29 +3,44 @@
 const hexColor = document.querySelector('.hex');
 const rgbColor = document.querySelector('.rgb');
 const generateBtn = document.querySelector('.btn');
+// const secondBtn = document.querySelector('.btn2');
 const colorPallate = document.querySelector('.color-pallate');
 const colorCode = document.querySelector('.color-codes')
 
 function randomRgb(){
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     let newColor = `rgb(${r}, ${g}, ${b})`;
     return newColor;
-}
 
-function newhexColor(){
-    let hexaColor = Math.floor(Math.random() * 16777215);
-    let newHexColor = `#${hexaColor.toString(16)}`;
+    function newhexColor(){
+    let r = Math.floor(Math.random() * 256).toString(16);
+    let g = Math.floor(Math.random() * 256).toString(16);
+    let b = Math.floor(Math.random() * 256).toString(16);
+    r = (r.length === 2) ? r : '0' + r;
+    g = (g.length === 2) ? g : '0' + g;
+    b = (b.length === 2) ? b : '0' + b;
+    let newHexColor = `#${r}${g}${b}`;
     return newHexColor;
 }
+}
+
+
 
 generateBtn.addEventListener('click', () => {
     let color = randomRgb();
     rgbColor.innerText = color;
     colorPallate.style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
+    // let nextColor = newhexColor();
+    // hexColor.innerText = nextColor;
+})
+generateBtn.addEventListener('click', () => {
+    colorPallate.style.backgroundColor = color;
     let nextColor = newhexColor();
     hexColor.innerText = nextColor;
 })
+
 
 
